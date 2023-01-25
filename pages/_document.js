d5ace7cdaf039b7fb84a1e7f43219dd955752b52
AguzZDev <example@example.com>
2023-01-25 18:35:11 -0300

    remove react-ga package and set script of google analytics in document file

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,19 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id='${process.env.NEXT_PUBLIC_REACTGA_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '${process.env.NEXT_PUBLIC_REACTGA_ID}')`,
+          }}
+        />
         <link rel="shortcut icon" href="/logo.png" />
         <Metatags />
       </Head>
