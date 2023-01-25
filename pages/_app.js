05d8e4012328afe8fcacb1615939ced7ab5c1ad2
AguzZDev <example@example.com>
2023-01-25 18:37:45 -0300

    i forget remove import

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,9 @@
 import { AnimatePresence } from "framer-motion"
-import { useEffect } from "react"
-import ReactGA from "react-ga4"
 
 import DarkmodeProvider from "context/DarkmodeProvider"
 import "styles/globals.css"
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    ReactGA.initialize(process.env.NEXT_PUBLIC_REACTGA_ID)
-  }, [])
-
   return (
     <AnimatePresence exitBeforeEnter>
       <div className="bg" />
